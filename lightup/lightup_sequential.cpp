23ed87b7d41e5412392c626e3e7d9fc3233c86d9
Jasmine Chen <j.lin013@gmail.com>
2015-12-20 22:41:37 +0800

    Updated lightup Sequential

--- lightup/lightup_sequential.cpp
+++ lightup/lightup_sequential.cpp
@@ -7,38 +7,61 @@
 using namespace std;
 using namespace cv;
 
+vector<vector<int> > mat;
+
 
 void Video() {
 	// Setup video capture device
 	// Link it to the first capture device
 	VideoCapture captureVideo;
-	captureVideo.open("videoSmall.mp4");
+	captureVideo.open("videoLarge.mp4");
 
 	Mat frameFromVideo;
 	clock_t cnt = 0;
-	while(true){
+	while (true){
 		captureVideo >> frameFromVideo;
-		if( frameFromVideo.empty() ) break;
+		if (frameFromVideo.empty()) break;
 		//imshow("origin", frameFromVideo);
+		if (mat.empty()) {
+			mat.resize(frameFromVideo.rows);
+			for (int i = 0; i < frameFromVideo.rows; ++i) {
+				mat[i].resize(frameFromVideo.cols);
+				for (int j = 0; j < frameFromVideo.cols; ++j)
+					mat[i][j] = frameFromVideo.at<Vec3b>(i, j)[0];
+			}
+		}
 
 		clock_t last = clock();
 		for (int i = 0; i < frameFromVideo.rows; i++)
 			for (int j = 0; j < frameFromVideo.cols; j++)
 				//for(int k=0; k<3; ++k)
-				frameFromVideo.at<Vec3b>(i,j)[0] = min(
-					2*frameFromVideo.at<Vec3b>(i,j)[0] + 5,
-					255
-				);
-		cnt += clock() - last;
-		//imshow("outputCamera", frameFromVideo);
+				mat[i][j] += mat[i][j] >> 3;
+				
 
-		if( waitKey(30)>=0 ) break;
+
+		// getting max value
+		int max_val = 0;
+		for (int i = 0; i < frameFromVideo.rows; i++)
+			for (int j = 0; j < frameFromVideo.cols; j++) {
+				if (mat[i][j] > max_val)
+					max_val = mat[i][j];
+			}
+		
+		// normalizing
+		for (int i = 0; i < frameFromVideo.rows; i++)
+			for (int j = 0; j < frameFromVideo.cols; j++)
+				frameFromVideo.at<Vec3b>(i, j)[0] = mat[i][j] / max_val;
+		
+		cnt += clock() - last;
+		imshow("outputCamera", frameFromVideo);
+
+		if (waitKey(30) >= 0) break;
 	}
-	printf("%fms\n", 1.0*cnt / (1.0*CLOCKS_PER_SEC/1000.0));
+	printf("%fms\n", 1.0*cnt / (1.0*CLOCKS_PER_SEC / 1000.0));
 }
 
 int main(int argc, const char** argv){
-	if( CV_MAJOR_VERSION < 3 ) {
+	if (CV_MAJOR_VERSION < 3) {
 		puts("Advise you update to OpenCV3");
 	}
 	Video();
