da0a5d7dcf4148b167dbe2b46ab47b3528dd040f
Jasmine Chen <j.lin013@gmail.com>
2015-12-20 22:12:23 +0800

    Updated lightup pthread (Single frame partition)

--- lightup/lightup_pthread.cpp
+++ lightup/lightup_pthread.cpp
@@ -11,18 +11,17 @@
 using namespace std;
 using namespace cv;
 
+vector<vector<int> > mat;
 Mat frameFromVideo;
 
 void lightUp(int tid) {
-	int threadPerNum = frameFromVideo.rows/threadNum;
+	int threadPerNum = frameFromVideo.rows / threadNum;
 	int from = tid * threadPerNum;
-	int to = (tid==threadNum-1)? frameFromVideo.rows : from+threadPerNum;
-	for(int i=from; i<to; ++i)
-		for(int j=0; j<frameFromVideo.cols; ++j)
-			frameFromVideo.at<Vec3b>(i,j)[0] = min(
-				(frameFromVideo.at<Vec3b>(i,j)[0]<<1) + 5,
-				255
-			);
+	int to = (tid == threadNum - 1) ? frameFromVideo.rows : from + threadPerNum;
+	for (int i = from; i < to; ++i)
+		for (int j = 0; j < frameFromVideo.cols; ++j)
+			for (int k = 0; k < ((i + j) >> 7); ++k)
+				mat[i][j] += mat[i][j] >> 3;
 }
 
 void Video() {
@@ -33,27 +32,52 @@ void Video() {
 	captureVideo.open("videoLarge.mp4");
 
 	clock_t cnt = 0;
-	while(true) {
+	while (true) {
 		captureVideo >> frameFromVideo;
-		if( frameFromVideo.empty() ) break;
+		if (frameFromVideo.empty()) break;
 		//imshow("origin", frameFromVideo);
+		if (mat.empty()) // if it's empty, resize first !
+		{
+			mat.resize(frameFromVideo.rows);
+			for (int i = 0; i < frameFromVideo.rows; ++i)
+				mat[i].resize(frameFromVideo.cols);
+		}
+		for (int i = 0; i < frameFromVideo.rows; ++i)
+			for (int j = 0; j < frameFromVideo.cols; ++j)
+				mat[i][j] = frameFromVideo.at<Vec3b>(i, j)[0];
 
+		
 		clock_t last = clock();
 		vector<thread> threads;
-		for(int i=0; i<threadNum; ++i)
+		for (int i = 0; i<threadNum; ++i)
 			threads.emplace_back(thread(lightUp, i));
-		for(int i=0; i<threadNum; ++i)
+		for (int i = 0; i<threadNum; ++i)
 			threads[i].join();
 		cnt += clock() - last;
-		//imshow("outputCamera", frameFromVideo);
+		
 
-		if( waitKey(30)>=0 ) break;
+		// getting max value
+		int max_val = 0;
+		for (int i = 0; i < frameFromVideo.rows; i++)
+			for (int j = 0; j < frameFromVideo.cols; j++) {
+				if (mat[i][j] > max_val)
+					max_val = mat[i][j];
+			}
+
+		// normalizing
+		for (int i = 0; i < frameFromVideo.rows; i++)
+			for (int j = 0; j < frameFromVideo.cols; j++)
+				frameFromVideo.at<Vec3b>(i, j)[0] = mat[i][j] / max_val;
+
+		imshow("outputCamera", frameFromVideo);
+
+		if (waitKey(30) >= 0) break;
 	}
-	printf("%fms\n", 1.0*cnt / (1.0*CLOCKS_PER_SEC/1000.0));
+	printf("%fms\n", 1.0*cnt / (1.0*CLOCKS_PER_SEC / 1000.0));
 }
 
 int main(int argc, const char** argv){
-	if( CV_MAJOR_VERSION < 3 ) {
+	if (CV_MAJOR_VERSION < 3) {
 		puts("Advise you update to OpenCV3");
 	}
 	Video();
